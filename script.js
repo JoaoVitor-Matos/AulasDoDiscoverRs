3f34f875819babd768c027fe25aad742fcb1039a
Jotinha017 <102634167+Jotinha017@users.noreply.github.com>
2023-07-07 17:15:21 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//jeito simplificado:
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  }
+  //jeito não simplificado:
+  //function toggleMode() {: jeito não simplificado
+  // const html = document.documentElement
+  //if (html.classList.contains("light")) {
+  //html.classList.remove("light")
+  //} else {
+  //html.classList.add("light")
+  //}
+  //}
+  
